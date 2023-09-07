a5c04a61b5b7dd312d54a503d1aebcf4ad1b587d
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-09-07 23:42:58 +0200

    hw-03

---
+++ controller/contacts.js
@@ -0,0 +1,109 @@
+const {
+    getAllContacts,
+    getByIdContact,
+    createContacts,
+    updateContact,
+    deleteContact,
+    updateStatusContact
+  } = require('./../service/index'); 
+  
+  const getContacts = async (req, res, next) => {
+    try {
+      const results = await getAllContacts()
+      res.status(200).json(results)
+    } catch (error) {
+      console.error(error);
+      next(error)
+    }
+  };
+  
+  const getContactById = async (req, res, next) => {
+    const { contactId } = req.params;
+    try {
+      const result = await getByIdContact(contactId);
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({
+          status: 'error',
+          code: 404,
+          message: `Not found task id: ${contactId}`,
+          data: 'Not Found',
+        });
+      }
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  };
+
+  const addContacts = async (req, res, next) => {
+    try {
+      const contact = await createContacts(req.body)
+  
+      res.status(201).json(contact)
+    } catch (error) {
+      next(error)
+    }
+  }; 
+
+  const removeContact = async (req, res, next) => {
+    try {
+      const result = await deleteContact(req.params.contactId)
+  
+      if (!result) {
+        return res.status(404).json({ message: 'Not found' })
+      }
+  
+      res.status(200).json({ message: 'contact deleted' })
+    } catch (error) {
+      next(error)
+    }
+  }; 
+  
+  
+  
+  const patchContact = async (req, res, next) => {
+    try {
+      const contact = await updateContact(req.params.contactId, req.body)
+  
+      if (!contact) {
+        return res.status(404).json({ message: 'Not found' })
+      }
+  
+      res.status(200).json(contact)
+    } catch (error) {
+      next(error)
+    }
+  }; 
+  
+  const changeStatusContact = async (req, res, next) => {
+    try {
+      const { contactId } = req.params;
+      const { favorite } = req.body;
+  
+      if (favorite === undefined) {
+        return res.status(400).json({ message: 'missing field favorite' });
+      }
+  
+      const updatedContact = await updateStatusContact(contactId, favorite);
+  
+      if (!updatedContact) {
+        return res.status(404).json({ message: 'Not found' });
+      }
+  
+      res.status(200).json(updatedContact);
+    } catch (error) {
+      next(error);
+    }
+  }; 
+  
+  module.exports = {
+      getContacts,
+      getContactById,
+      addContacts,
+      removeContact,
+      patchContact,
+      changeStatusContact,
+      
+  }
--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 const app = require('./app')
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.CONECTION__STRING;
+
+const connection = mongoose.connect(uriDb, { dbName: "db-contacts" });
+
+
+connection
+  .then(() => {
+    console.log('Database connection successful');
+    app.listen(PORT, function () {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log(`Server not running. Error message: ${err.message}`);
+    process.exit(1);
+  })
+
+
+
+  // app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
